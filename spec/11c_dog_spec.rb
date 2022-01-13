514f432f40bc80a9794549921e44982bb61f9d3f
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-13 09:01:11 -0700

    Complete 11_pet_spec

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
+  subject(:anju) { described_class.new('Anju', 'German Shepherd', 'tan') }
 
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'when Dog and Cat both respond to :talk' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(anju).not_to be_barking
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(anju).to be_sleeping
   end
 end
